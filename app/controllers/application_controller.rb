bf93a3e046eb40c046cc9971d5ff179184049c00
kimu2273 <90764332+kimu2273@users.noreply.github.com>
2021-10-04 12:12:18 +0900

    新規登録機能の実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
   private
@@ -8,4 +9,9 @@ class ApplicationController < ActionController::Base
       username == 'mik' && password == '2273'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day])
+  end
+
 end
