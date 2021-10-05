cfa8e3f380b2202489d8dae745ccc5460feedb9b
kimu2273 <90764332+kimu2273@users.noreply.github.com>
2021-10-05 13:53:56 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_charges
   belongs_to :shipping_days
   has_one_attached :image
+  belongs_to :user
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :condition_id, numericality: { other_than: 1 } 
   validates :Shipping_area_id, numericality: { other_than: 1 } 
   validates :shipping_charges_id, numericality: { other_than: 1 } 
   validates :shipping_days_id, numericality: { other_than: 1 } 
+  validates :name, :explanation, :price, :image, presence: true
 end
