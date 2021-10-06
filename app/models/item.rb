372421192f53a41ddcb5d92eb26a32b7c0ca5a1b
kimu2273 <90764332+kimu2273@users.noreply.github.com>
2021-10-06 11:28:05 +0900

    エラーメッセージの調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :caegory
+  belongs_to :category
   belongs_to :condition
   belongs_to :shipping_area
   belongs_to :shipping_charges
@@ -8,10 +8,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :shipping_area_id, numericality: { other_than: 1 } 
-  validates :shipping_charges_id, numericality: { other_than: 1 } 
-  validates :shipping_days_id, numericality: { other_than: 1 } 
   validates :name, :explanation, :price, :image, presence: true
+  validates :price, numericality: {in: 300..9999999, message: "is out of setting range"}, format: {with: /\A(?=.*?[\d])+\z/i, message: "is invalid. Input half-width characters"}
+  validates :category_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :shipping_area_id, numericality:  { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_charges_id, numericality:  { other_than: 1 , message: "can't be blank"}
+  validates :shipping_days_id, numericality:  { other_than: 1 , message: "can't be blank"}
 end
