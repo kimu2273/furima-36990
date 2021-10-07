901fdbd5837c65653d3f0dc88a1e86bb24e9cd63
kimu2273 <90764332+kimu2273@users.noreply.github.com>
2021-10-07 11:20:42 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if user_signed_in?&& current_user.id == @item.user_id
+      edit_item_path(@item.id)
+    elsif user_signed_in?
+      redirect_to root_path
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def item_params
